6e6e967c8d6e68c60a071a19348a8d9f3280c3c9
Arafat Zahan <kuasha420@gmail.com>
2020-02-15 03:15:55 +0600

    fixed reset button behaviorr

--- assets/custom/scripts.js
+++ assets/custom/scripts.js
@@ -1,122 +1,117 @@
 $(document).ready(function() {
+	// smooth anchor scrolling
+	$(document).on('click', 'a[href^="#"]', function(event) {
+		event.preventDefault();
 
-  // smooth anchor scrolling
-  $(document).on('click', 'a[href^="#"]', function (event) {
-    event.preventDefault();
+		$('html, body').animate(
+			{
+				scrollTop: $($.attr(this, 'href')).offset().top
+			},
+			500
+		);
+	});
 
-    $('html, body').animate({
-        scrollTop: $($.attr(this, 'href')).offset().top
-    }, 500);
-  });
+	// fix menu when passed
+	$('.masthead').visibility({
+		once: false,
+		onBottomPassed: function() {
+			$('.fixed.menu').transition('fade in');
+		},
+		onBottomPassedReverse: function() {
+			$('.fixed.menu').transition('fade out');
+		}
+	});
 
-  // fix menu when passed
-  $('.masthead').visibility({
-      once: false,
-      onBottomPassed: function() {
-        $('.fixed.menu').transition('fade in');
-      },
-      onBottomPassedReverse: function() {
-        $('.fixed.menu').transition('fade out');
-      }
-    });
+	// create sidebar and attach to menu open
+	$('.ui.sidebar').sidebar('attach events', '.toc.item');
 
-  // create sidebar and attach to menu open
-  $('.ui.sidebar').sidebar('attach events', '.toc.item');
+	// Forn validation
+	$('.ui.form').form({
+		fields: {
+			fname: {
+				identifier: 'fname',
+				rules: [
+					{
+						type: 'empty',
+						prompt: 'Please enter your first name.'
+					}
+				]
+			},
+			lname: {
+				identifier: 'lname',
+				rules: [
+					{
+						type: 'empty',
+						prompt: 'Please enter your last name.'
+					}
+				]
+			},
+			email: {
+				identifier: 'email',
+				rules: [
+					{
+						type: 'empty',
+						prompt: 'Please enter your email address.'
+					},
+					{
+						type: 'email',
+						prompt: 'Please enter a valid email address.'
+					}
+				]
+			},
+			subject: {
+				identifier: 'subject',
+				rules: [
+					{
+						type: 'empty',
+						prompt: 'Subject can not be empty.'
+					}
+				]
+			},
+			message: {
+				identifier: 'message',
+				rules: [
+					{
+						type: 'empty',
+						prompt: 'Please enter your message.'
+					}
+				]
+			}
+		},
+		onSuccess: function(e) {
+			e.preventDefault();
+			$('.ui.form').toggleClass('double loading');
 
-    // Forn validation
-    $('.ui.form')
-    .form({
-        fields: {
-        fname: {
-            identifier: 'fname',
-            rules: [
-            {
-                type   : 'empty',
-                prompt : 'Please enter your first name.'
-            }
-            ]
-        },
-        lname: {
-            identifier: 'lname',
-            rules: [
-            {
-                type   : 'empty',
-                prompt : 'Please enter your last name.'
-            }
-            ]
-        },
-        email: {
-            identifier: 'email',
-            rules: [
-            {
-                type   : 'empty',
-                prompt : 'Please enter your email address.'
-            },
-            {
-                type   : 'email',
-                prompt : 'Please enter a valid email address.'
-            }
-            ]
-        },
-        subject: {
-            identifier: 'subject',
-            rules: [
-            {
-                type   : 'empty',
-                prompt : 'Subject can not be empty.'
-            }
-            ]
-        },
-        message: {
-            identifier: 'message',
-            rules: [
-            {
-                type   : 'empty',
-                prompt : 'Please enter your message.'
-            }
-            ]
-        }
-        },
-        onSuccess: function(e) {
+			var href = $(this).attr('action');
 
-            e.preventDefault();
-            $('.ui.form').toggleClass("double loading");
-            
-            var href = $(this).attr("action");
+			$.ajax({
+				type: 'POST',
 
-            $.ajax({
+				dataType: 'json',
 
-                type: "POST",
+				url: href,
 
-                dataType: "json",
+				data: $(this).serialize(),
 
-                url: href,
+				success: function(response) {
+					if (response.status == 'success') {
+						console.log('message sent');
 
-                data: $(this).serialize(),
+						$('.ui.form').toggleClass('double loading');
+						$('.ui.form').addClass('succuss');
+					} else {
+						console.log('message sending failed');
 
-                success: function(response){
+						$('.ui.form').toggleClass('double loading');
+						$('.ui.form').addClass('error');
+					}
+				}
+			});
+		}
+	});
 
-                    if(response.status == "success"){
-
-                        console.log("message sent");
-                        
-                        $('.ui.form').toggleClass("double loading");
-                        $('.ui.form').addClass("succuss");
-
-                    }else{
-
-                        console.log("message sending failed");
-
-                        $('.ui.form').toggleClass("double loading");
-                        $('.ui.form').addClass("error");
-
-                    }
-
-                }
-
-            });
-
-        }
-    });
-
+	$('#formReset').click(function(e) {
+		$('.ui.form').children('.inline.fields').children('.error').removeClass('error');
+		$('.ui.form').removeClass('error');
+	});
 });
